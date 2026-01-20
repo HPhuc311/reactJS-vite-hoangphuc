05f814497d89ae1b5b27e607e118eb16c822c588
Hoang Phuc <hoangphuc311tn@gmail.com>
2026-01-20 14:57:25 +0700

    video31

--- src/assets/components/learn/MyComponent.jsx
+++ src/assets/components/learn/MyComponent.jsx
@@ -6,9 +6,19 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const hoidanit = "Douple Hello"; //string
+    // const hoidanit = 25; // number
+    // const hoidanit = true; // boolean
+    const hoidanit = [1 , 2 , 4]; 
+    // const hoidanit = {
+    //     name: "Phuc",
+    //     age: 18
+    // } Object
+
     return (
         <>
-            <div>Hello Mấy Phen Update </div>
+            <div> {JSON.stringify(hoidanit)}Hello Mấy Phen Update </div>
+            <div>{console.log("hoangphuc")}</div>
             <div className="child" style={{
                 borderRadius: "10px"
             }}>child</div>
