4790dd0fdd51ed229f8886492dffcf61b3dc3378
Hoang Phuc <hoangphuc311tn@gmail.com>
2026-01-20 08:42:00 +0700

    video30

--- src/assets/components/learn/MyComponent.jsx
+++ src/assets/components/learn/MyComponent.jsx
@@ -1,10 +1,19 @@
 //arrow function 
 // const tên () => {}
 // component = HTML + CSS + JS
+// JSX
+// fragment     
+import './style.css';
+
 const MyComponent = () => {
-  return (
-    <div>Hello Mấy Phen Update</div>
-  );
+    return (
+        <>
+            <div>Hello Mấy Phen Update </div>
+            <div className="child" style={{
+                borderRadius: "10px"
+            }}>child</div>
+        </>
+    );
 }
 
 export default MyComponent
