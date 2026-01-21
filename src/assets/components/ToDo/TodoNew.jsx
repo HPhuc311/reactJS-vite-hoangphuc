b68fea844ddfcd2055526915aef330b63d7bd188
Hoang Phuc <hoangphuc311tn@gmail.com>
2026-01-21 10:34:12 +0700

    video36

--- src/assets/components/ToDo/TodoNew.jsx
+++ src/assets/components/ToDo/TodoNew.jsx
@@ -1,11 +1,24 @@
 const TodoNew = (props) => {
-const {addNewTodo} = props;
+    const { addNewTodo } = props;
 
-addNewTodo("hoangphuc")
+    // addNewTodo("hoangphuc")
+    const handleClick = () => {
+        alert("Click Me")
+    }
+    // lấy giá trị ô input
+    const handleChange = (name) => {
+        console.log(">>>> handle on change", name)
+    }
+
     return (
         <div className='todo-new'>
-            <input type="text" placeholder='Enter your task' />
-            <button>Add</button>
+            <input type="text" placeholder='Enter your task' 
+            onChange={(event) => {handleChange(event.target.value)}}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     );
 }
