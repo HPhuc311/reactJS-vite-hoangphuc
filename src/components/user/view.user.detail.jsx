a8463ba1e04526796a83db77e2cb6f9e2178a042
Hoang Phuc <hoangphuc311tn@gmail.com>
2026-01-26 17:50:08 +0700

    video82

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,11 +1,12 @@
-import { Drawer } from "antd";
+import { Button, Drawer } from "antd";
 
 const ViewUserDetails = (props) => {
 
     const { isDetailOpen, setIsDetailOpen, dataDetail, setDataDetail } = props;
 
-    return(
+    return (
         <Drawer
+            width={"40vw"}
             title="Basic Drawer"
             closable={{ 'aria-label': 'Close Button' }}
             onClose={() => {
@@ -14,25 +15,45 @@ const ViewUserDetails = (props) => {
             }}
             open={isDetailOpen}
         >
-            {dataDetail ? 
-            <>
-                <p>ID: {dataDetail._id}</p>
-                <br />
-                <p>Full Name: {dataDetail.fullName}</p>
-                <br />
-                <p>Email: {dataDetail.email}</p>
-                <br />
-                <p>Phone Number: {dataDetail.phone}</p>
-                <br />
-            
-            </>:
-            <>
-            <p>Không có dữ liệu nèo</p>
-            </>
+            {dataDetail ?
+                <>
+                    <p>ID: {dataDetail._id}</p>
+                    <br />
+                    <p>Full Name: {dataDetail.fullName}</p>
+                    <br />
+                    <p>Email: {dataDetail.email}</p>
+                    <br />
+                    <p>Phone Number: {dataDetail.phone}</p>
+                    <br />
+                    <p>Avatar:</p>
+                    <br />
+                    <div >
+                        <img height={100} width={150}
+                            src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} alt="" />
+                    </div>
+                    <div>
+                        <label htmlFor="btnInput"
+                        style={{
+                            display: "block",
+                            width: "fit-content",
+                            marginTop: "15px",
+                            padding: "5px 10px",
+                            background: "orange",
+                            borderRadius: "5px",
+                            cursor: "pointer"
+                        }}
+                        >Upload a Avatar</label>
+                        <input type="file" id="btnInput" hidden />
+                    </div>
+                    {/* <Button type="primary">Upload a Avatar</Button> */}
+                </> :
+                <>
+                    <p>Không có dữ liệu nèo</p>
+                </>
 
             }
         </Drawer>
     );
 }
- 
+
 export default ViewUserDetails;
